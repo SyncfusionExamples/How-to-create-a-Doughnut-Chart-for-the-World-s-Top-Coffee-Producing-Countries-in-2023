12f3ad8db48f66c3cfacf055ddd9f7ad0cb9adb2
NanthiniMahalingam <nanthini.mahalingam@syncfusion.com>
2023-06-29 12:28:55 +0530

    Commit the changes.

--- CoffeeProductionChart/ViewModel/WorldCoffeeProduction.cs
+++ CoffeeProductionChart/ViewModel/WorldCoffeeProduction.cs
@@ -8,6 +8,9 @@ namespace CoffeeProductionChart
     {
         List<CoffeeProductionModel> productionDetails;
         public List<Brush> PaletteBrushes { get; set; }
+        public List<Brush> LightBrushes { get; set; }
+        public List<Brush> SelectionBrushes { get; set; }
+        public List<Brush> SelectionBrushes1 { get; set; }
 
         public List<CoffeeProductionModel> ProductionDetails
         {
@@ -19,6 +22,22 @@ namespace CoffeeProductionChart
             {
                 productionDetails = value;
                 NotifyPropertyChanged(nameof(ProductionDetails));
+            }
+        }
+
+
+        private Brush textColor;
+        public Brush TextColor
+        {
+            get
+            {
+                return textColor;
+            }
+            set
+            {
+                textColor = value;
+
+                NotifyPropertyChanged(nameof(TextColor));
             }
         }
 
@@ -75,6 +94,7 @@ namespace CoffeeProductionChart
             set
             {
                 selectedIndex = value;
+                TextColor = SelectionBrushes1[SelectedIndex];
                 UpdateIndex(value);
                 NotifyPropertyChanged(nameof(SelectedIndex));
             }
@@ -90,15 +110,53 @@ namespace CoffeeProductionChart
         public WorldCoffeeProduction()
         {
             ProductionDetails = new List<CoffeeProductionModel>(ReadCSV());
+
+            LightBrushes = new List<Brush>
+            {
+                new SolidColorBrush(Color.FromArgb("#5bdffc")),
+                new SolidColorBrush(Color.FromArgb("#2fe0d0")),
+
+                new SolidColorBrush(Color.FromArgb("#baf74d")),
+                new SolidColorBrush(Color.FromArgb("#f5d949")),
+                new SolidColorBrush(Color.FromArgb("#f2464d")),
+                new SolidColorBrush(Color.FromArgb("#c86af7"))
+            };
+
             PaletteBrushes = new List<Brush>
             {
                 new SolidColorBrush(Color.FromArgb("#02daf7")),
-                new SolidColorBrush(Color.FromArgb("#8ebf24")),
+                new SolidColorBrush(Color.FromArgb("#34f7db")),
+
+                new SolidColorBrush(Color.FromArgb("#cbf536")),
                 new SolidColorBrush(Color.FromArgb("#60789b")),
-                new SolidColorBrush(Color.FromArgb("#E14A4A")),
-                new SolidColorBrush(Color.FromArgb("#f48945")),
+                new SolidColorBrush(Color.FromArgb("#f55c31")),
                 new SolidColorBrush(Color.FromArgb("#91979E"))
             };
+
+
+            SelectionBrushes = new List<Brush>
+            {
+                new SolidColorBrush(Color.FromArgb("#02cef7")),
+                new SolidColorBrush(Color.FromArgb("#14917f")),
+
+                new SolidColorBrush(Color.FromArgb("#E14A4A")),
+                new SolidColorBrush(Color.FromArgb("#60709b")),
+                new SolidColorBrush(Color.FromArgb("#f27627")),
+                new SolidColorBrush(Color.FromArgb("#757980"))
+            };
+
+            SelectionBrushes1 = new List<Brush>
+            {
+                new SolidColorBrush(Color.FromArgb("#00b1d9")),
+                new SolidColorBrush(Color.FromArgb("#05b3a2")),
+
+                new SolidColorBrush(Color.FromArgb("#8ccf15")),
+                new SolidColorBrush(Color.FromArgb("#d9b709")),
+                new SolidColorBrush(Color.FromArgb("#d40b13")),
+                new SolidColorBrush(Color.FromArgb("#9e07e3"))
+            };
+
+
         }
 
         public IEnumerable<CoffeeProductionModel> ReadCSV()
